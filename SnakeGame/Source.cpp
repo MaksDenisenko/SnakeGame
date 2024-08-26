e3228fd50cbe1495d356b2bbfd4226957e4069c3
Max Denisenko <maksim.denisenko.20@mail.ru>
2024-08-26 16:51:09 +0300

    Инициализация змейки

--- SnakeGame/Source.cpp
+++ SnakeGame/Source.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+#include <random>
 using namespace std;
+
+struct snakePiece {
+	int x, y;
+	snakePiece* next;
+	snakePiece* prev;
+};
 
 int main() {
 
@@ -15,6 +21,33 @@ int main() {
 		cout << "Высота: ";
 		cin >> fieldHeight;
 	} while ((fieldHeight <= 2) || (fieldWidht <= 2));
+	
+	//Создание поля
+	char** field = new char*[fieldHeight];
+	for (int i = 0; i < fieldHeight; i++) {
+		field[i] = new char[fieldWidht];
+	}
+
+
+	for (int i = 0; i < fieldHeight; i++) {
+		for (int j = 0; j < fieldWidht; j++) {
+			field[i][j] = ' ';
+		}
+	}
+	system("cls");
+	for (int i = 0; i < fieldHeight; i++) {
+		for (int j = 0; j < fieldWidht; j++) {
+			cout << field[i][j];
+		}
+		cout << endl;
+	}
+
+	snakePiece snake;
+	snakePiece sneakHead;
+
+	//Голова змеи ставится в рандомное место
+	snake.x = rand() % fieldWidht;
+	snake.y = rand() % fieldHeight;
 
 	return 0;
 }
