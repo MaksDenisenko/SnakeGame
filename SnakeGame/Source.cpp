b971630e90ac0c432745b7df8e3a8660eaabf13f
Max Denisenko <maksim.denisenko.20@mail.ru>
2024-08-27 17:43:39 +0300

    Работает движение головы змейки

--- SnakeGame/Source.cpp
+++ SnakeGame/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <conio.h>
 using namespace std;
 
 struct snakePiece {
@@ -25,6 +26,10 @@ void clearField(char** field, int fieldHeight, int fieldWidth) {
 	}
 }
 
+void setFigures(char** field, int fieldHeight, int fieldWigth, snakePiece* snake) {
+	
+}
+
 int main() {
 
 	setlocale(LC_ALL, "Russian");
@@ -45,7 +50,7 @@ int main() {
 		field[i] = new char[fieldWidth];
 	}
 
-
+	clearField(field, fieldHeight, fieldWidth);
 	system("cls");
 	for (int i = 0; i < fieldHeight; i++) {
 		for (int j = 0; j < fieldWidth; j++) {
@@ -55,14 +60,49 @@ int main() {
 	}
 
 	snakePiece snake;
-	snakePiece sneakHead;
+	snakePiece* sneakHead = &snake;
 
 	//Голова змеи ставится в рандомное место
 	snake.x = rand() % fieldWidth;
 	snake.y = rand() % fieldHeight; 
 	snake.next = NULL;
 	snake.prev = NULL;
+	
+	bool lively = true;
+	char symbol; //Считывание символа, введенного пользователем
 
+	int currentLength = 0;
+
+	while (lively) {
+		symbol = _getch();
+		
+		if (symbol == 'w') {
+			snake.y -= 1;
+			if (snake.y < 0) snake.y = fieldHeight - 1;
+		}
+		if (symbol == 'd') {
+			snake.x = (snake.x + 1) % fieldWidth;
+		}
+		if (symbol == 's') {
+			snake.y = (snake.y + 1) % fieldHeight;
+		}
+		if (symbol == 'a') {
+			snake.x -= 1;
+			if (snake.x < 0) snake.x = fieldWidth - 1;
+		}
+		snakePiece* snakeCurrent = sneakHead;
+		currentLength = 0;
+		clearField(field, fieldHeight, fieldWidth);
+		while (snakeCurrent != NULL) {
+			if (currentLength == 0) field[snakeCurrent->y][snakeCurrent->x] = 'O';
+			else field[snakeCurrent->y][snakeCurrent->x] = '@';
+
+			currentLength++;
+			snakeCurrent = snakeCurrent->next;
+		}
+		system("cls");
+		printField(field, fieldHeight, fieldWidth);
+	}
 
 	return 0;
 }
